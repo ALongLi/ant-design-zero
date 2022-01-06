4046bd6d60a132d1d5df45422f044f9786559a11
Liam <liam.wang@live.com>
2022-01-06 11:19:57 +0800

    style: login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -15,54 +15,59 @@ const mockWait = (time = 100) => {
 export default () => {
   const auth = useAuth()
   return (
-    <div className={styles.login}>
-      <LoginForm
-        title="Ant Design Zero"
-        subTitle="A starting point for new web applications."
-        actions={
-          <>
-            其他登录方式
-            <WechatOutlined className={styles.openicon} />
-          </>
-        }
-        request={async () => {
-          await mockWait(200)
-          return { username: '', password: '' }
-        }}
-        onFinish={async (values: LoginForm) => {
-          await auth.login(values)
-        }}
-      >
-        <ProFormText
-          name="username"
-          fieldProps={{
-            size: 'large',
-            prefix: <UserOutlined className={'prefixIcon'} />,
-          }}
-          placeholder={'用户名: admin'}
-          rules={[
-            {
-              required: true,
-              message: '请输入用户名',
-            },
-          ]}
-        />
-        <ProFormText.Password
-          name="password"
-          fieldProps={{
-            size: 'large',
-            prefix: <LockOutlined className={'prefixIcon'} />,
-          }}
-          placeholder={'密码: admin'}
-          rules={[
-            {
-              required: true,
-              message: '请输入密码',
-            },
-          ]}
-        />
-      </LoginForm>
-      <Footer />
-    </div>
+    <>
+      <div className={styles.bg}></div>
+      <div className={styles.login}>
+        <div className={styles.loginForm}>
+          <LoginForm
+            title="Ant Design Zero"
+            subTitle="A starting point for new web applications."
+            actions={
+              <>
+                其他登录方式
+                <WechatOutlined className={styles.openicon} />
+              </>
+            }
+            request={async () => {
+              await mockWait(200)
+              return { username: '', password: '' }
+            }}
+            onFinish={async (values: LoginForm) => {
+              await auth.login(values)
+            }}
+          >
+            <ProFormText
+              name="username"
+              fieldProps={{
+                size: 'large',
+                prefix: <UserOutlined className={'prefixIcon'} />,
+              }}
+              placeholder={'用户名: admin'}
+              rules={[
+                {
+                  required: true,
+                  message: '请输入用户名',
+                },
+              ]}
+            />
+            <ProFormText.Password
+              name="password"
+              fieldProps={{
+                size: 'large',
+                prefix: <LockOutlined className={'prefixIcon'} />,
+              }}
+              placeholder={'密码: admin'}
+              rules={[
+                {
+                  required: true,
+                  message: '请输入密码',
+                },
+              ]}
+            />
+          </LoginForm>
+        </div>
+        <Footer />
+      </div>
+    </>
   )
 }
